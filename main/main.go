4208bba43c391e6bcda3089a3857011e29161b9b
wanll <emiteech@gmail.com>
2026-04-19 14:24:42 -0300

    feat: enhance movie management with add_movies functionality

--- main/main.go
+++ main/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 type movie struct {
 	name string
 	quantity int 
 }
+
+const space = "================================"
 
 var movies []movie
 func main () {
@@ -18,8 +23,34 @@ func main () {
 		{name: "Before sunrise", quantity: 0},
 	} 
 
+	fmt.Print(space)
+	get_movies(movies)
+	fmt.Println(space)
+	movies = add_movies(movies)
+	fmt.Println(space)
 	get_movies(movies)
 
+}
+
+func add_movies(m []movie) []movie {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("which movie do you want to add (name): ")
+	scanner.Scan()
+	name := scanner.Text()
+
+	fmt.Print("quntity available (number): ")
+	scanner.Scan()
+	qtdStr := scanner.Text()
+
+	quantity, err := strconv.Atoi(qtdStr)
+	if err != nil {
+		fmt.Print("Something went wrong")
+		return m
+	}
+
+	m = append(m, movie{name, quantity})
+	
+	return m
 }
 
 func get_movies(m []movie) {
